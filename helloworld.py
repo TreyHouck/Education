fb1ceb3b3d17c311874f1f4e789cf4c05e5d349d
TreyHouck <148604314+TreyHouck@users.noreply.github.com>
2023-10-21 16:33:05 -0700

    Add files via upload
    Helloworld function making use of the printf command and string variables.

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# This program says hello and asks for your name. Its intent is to demonstrate some basic python principles and the usage of printf.
+print('Hello, world!')
+print('What is your name?')
+name=input()
+print(f'It is nice to meet you {name}, have a wonderful day')
+print(f'{name}, may I ask how old you are?')
+age=input()
+print(f'Thank you {name}, I now know you are {age} years old.')
