fe7ff9862d94e24aae484ac8c07937abfc79ae24
Trey Houck <treyhouck111@gmail.com>
2025-04-16 12:30:59 -0700

    Delete 0771-jewels-and-stones directory

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return sum(x in jewels for x in stones)
-        
-        ##So the way I understand this one, is that we are using a list to find the number of combinations in jewels before then searching for those jewl combinations in stones.
