e7ab831eb62ff1e3bd5ed5bb6d8c7f2c948891ec
Trey Houck <treyhouck111@gmail.com>
2025-04-16 12:27:32 -0700

    Delete 1678-goal-parser-interpretation directory

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def interpret(self, command: str) -> str:
-       return command.replace('()','o').replace('(al)','al')
-
-       #Takeaway: Commands can be chained with periods
