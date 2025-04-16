51173ae7772f5e1e055ef2e0e36adcdc0aea05cd
Trey Houck <treyhouck111@gmail.com>
2025-04-16 12:27:17 -0700

    Delete 1816-truncate-sentence directory

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def truncateSentence(self, s: str, k: int) -> str:
-        wordlist=s.split(" ")
-        truncated_list=wordlist[:k]
-        s = " ".join(truncated_list)
-        return s
