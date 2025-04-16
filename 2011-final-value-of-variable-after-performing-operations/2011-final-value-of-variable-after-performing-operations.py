8520b6d106e11109156242c2b5971a385608677f
Trey Houck <treyhouck111@gmail.com>
2025-04-16 12:27:10 -0700

    Delete 2011-final-value-of-variable-after-performing-operations directory

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def finalValueAfterOperations(self, operations: List[str]) -> int:
-        x = 0
-        for X in operations:
-            if X[1] == "+":
-                x += 1
-            else: 
-                x -= 1
-        return x
